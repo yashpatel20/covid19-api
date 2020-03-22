ba4486947d174862d6ec9eeda9f57d7b9d4de97c
Yashpatel20 <yash.work10@gmail.com>
2020-03-22 13:55:19 +0530

    changed cron job to 30 mins

--- utils/cron.js
+++ utils/cron.js
@@ -1,7 +1,7 @@
 const cron = require("node-cron");
 const scrape = require("../scraper");
 
-cron.schedule("* * * * *", () => {
+cron.schedule("*/30 * * * *", () => {
   console.log("running cron");
   scrape("https://www.mohfw.gov.in/");
 });
