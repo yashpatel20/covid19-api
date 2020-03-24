de8dc7f94e751028c9a8b79f66b274581922cb99
Yashpatel20 <yash.work10@gmail.com>
2020-03-24 15:59:00 +0530

    deploy

--- utils/cron.js
+++ utils/cron.js
@@ -1,11 +1,12 @@
 const cron = require("node-cron");
 const scrape = require("./scraper");
 
-cron.schedule("* * * * *", () => {
-  console.log("running cron");
-  scrape("https://www.mohfw.gov.in/");
-});
-// cron.schedule("*/15 * * * *", () => {
+// cron.schedule("* * * * *", () => {
 //   console.log("running cron");
 //   scrape("https://www.mohfw.gov.in/");
 // });
+
+cron.schedule("*/30 * * * *", () => {
+  console.log("running cron");
+  scrape("https://www.mohfw.gov.in/");
+});
