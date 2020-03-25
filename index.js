8e0724dd3412db373ec4777785a96dde356ed945
Yashpatel20 <yash.work10@gmail.com>
2020-03-25 15:16:32 +0530

    dyno keeps sleeping, will have to buy better hosting

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const app = require("./app");
 const http = require("http");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
-const wakeUpDyno = require("./utils/wakeUpDyno");
 
 const server = http.createServer(app);
 
 server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
-  wakeUpDyno("https://young-fjord-26148.herokuapp.com/", 7100000);
 });
